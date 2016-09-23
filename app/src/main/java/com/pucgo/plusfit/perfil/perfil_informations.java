ed2803b2ba7f130aadd77ad43e9c5935fc972086
AndersonSC <anderson87pa@gmail.com>
2016-09-22 22:26:11 -0300

    Iniciando o acesso de informações do perfil pelo menu.

---
+++ app/src/main/java/com/pucgo/plusfit/perfil/perfil_informations.java
@@ -0,0 +1,13 @@
+package com.pucgo.plusfit.perfil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class perfil_informations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_informations);
+    }
+}
